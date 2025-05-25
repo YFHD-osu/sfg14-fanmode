220cb281116b44cce678d296c8c87f27a2c15d03
YFHD-osu <ethan941106@gmail.com>
2025-05-25 11:50:32 +0800

    Update

--- README.md
+++ README.md
@@ -1,3 +1,72 @@
 # SFG14-73 Fan Control Utility
 
 Set fan speed strategy for ``Acer Swift Go 14 (SFG14-73)`` on Linux.
+
+> [!WARNING]  
+> Please ensure that your device model is ``SFG14-73``. Writing the wrong EC address to a different machine could cause hardware damage!
+
+## How to determine this program is sutiable for my device
+This program will read and write EC register to achieve get and set fan mode. \
+So the only thing we need to ensure is that the device uses the same EC register to store the fan mode. \
+To check this, you can follow this [wiki](https://github.com/hirschmann/nbfc/wiki/Probe-the-EC's-registers) 
+
+In my case (SFG14-73) the values are:
+```c
+#define EC_CMD_PORT  0x66
+#define EC_DATA_PORT 0x62
+#define EC_OFFSET 0x45
+```
+and fanmode:
+```c
+#define BALANCE_MODE 1
+#define SLIENT_MODE 2
+#define PERFORMANCE_MODE 3
+```
+
+## Installation 
+1. Clone this repository
+2. Compile the ``main.c`` file using ``gcc -o fanctl main.c`` to generate executable: ``fanctl``
+3. Move ``fanctl`` to your desired location or consider add it to path
+
+## Usage
+Use command ``fanctl -h`` to get help
+
+### Read Fanmode
+```bash
+sudo fanctl -r
+# Output: EC RAM value=3
+```
+
+### Set Fanmode
+```bash
+sudo ./fanctl -w 3
+# Output: Successfully set EC RAM 0x45 to 0x03
+```
+
+### Using with waybar
+By passing the ``-f waybar`` parameter, the program will output in waybar json format
+```bash
+sudo ./fanctl -f waybar -r
+# Output: {"value": 3, "text": "󰓅", "tooltip": "Fan Mode: Performance"}
+```
+
+## Waybar Setup
+To add this as a widget in waybar, please follow the step below
+
+1. Create an new entry in ``config.jsonc`` 
+```json
+"custom/fanmode": {
+  "exec": "sudo fanctl -r -f waybar",
+  "return-type": "json",
+  "format": "{}",
+  "on-click": "sudo fanctl -t && kill -s 44 $(pidof waybar)",
+  "min-length": 1,
+  "max-length": 1,
+  "signal": 10,  // Refresh when SIGRTMIN+10 is revieced
+}
+```
+
+2. Open passwordless sudo list using ``sudo visudo``, and add the code below  
+```bash
+<USERNAME> ALL=(ALL) NOPASSWD: /full/path/to/fanctl *
+```
--- main.c
+++ main.c
@@ -3,14 +3,37 @@
 #include <stdlib.h>
 #include <sys/io.h>
 #include <string.h>
+#include <locale.h>
 
 #define EC_CMD_PORT  0x66
 #define EC_DATA_PORT 0x62
 #define EC_OFFSET 0x45 // Write Reg 45 in EC RAM
 
-void show_help() {
+#define BALANCE_MODE 1
+#define SLIENT_MODE 2
+#define PERFORMANCE_MODE 3
+
+typedef enum opt_format {
+    waybar,
+    json
+} opt_format_t;
+
+typedef enum op_format {
+    help,
+    readEC,
+    writeEC,
+    toggleEC
+} op_format_t;
+
+typedef struct args {
+    unsigned char writeValue;
+    op_format_t opMode;
+    opt_format_t format;
+} args_t;
+
+void showHelp(char *path) {
     printf(
-        "Usage: nano [OPTIONS] [VALUE]\n"
+        "Usage: %s [OPTIONS] [VALUE]\n"
         "\n"
         "Option    Meaning\n"
         " -h       Show this help message\n"
@@ -18,10 +41,10 @@ void show_help() {
         " -w       Write fan mode to EC\n"
         "\n"
         "Writable value:\n"
-        " 1        Balance Mode\n"
-        " 2        Slient Mode\n"
-        " 3        Permormance Mode\n"
-    );
+        " %d        Balance Mode\n"
+        " %d        Slient Mode\n"
+        " %d        Permormance Mode\n",
+    path, BALANCE_MODE, SLIENT_MODE, PERFORMANCE_MODE);
     
     return;
 }
@@ -34,7 +57,7 @@ void wait_output_buffer_full() {
     while (!(inb(EC_CMD_PORT) & 0x01)); // Bit 0 = output buffer full
 }
 
-void ec_write(unsigned char value) {
+void ecWrite(unsigned char value) {
     // 取得對 I/O 埠的存取權限
     if (ioperm(EC_CMD_PORT, 1, 1) || ioperm(EC_DATA_PORT, 1, 1)) {
         perror("ioperm");
@@ -52,7 +75,7 @@ void ec_write(unsigned char value) {
     outb(value, EC_DATA_PORT);      // 寫入資料
 }
 
-unsigned char ec_read() {
+unsigned char ecRead() {
     // 取得 I/O 權限
     if (ioperm(EC_CMD_PORT, 1, 1) || ioperm(EC_DATA_PORT, 1, 1)) {
         perror("ioperm");
@@ -69,54 +92,163 @@ unsigned char ec_read() {
     return inb(EC_DATA_PORT); // 讀出資料
 }
 
-int main(int argc, char *argv[]) {
+args_t* parseArgs(int argc, char *argv[]) {
+    static args_t args = {help, 99, 99};
 
-    if (argc < 2 || argc > 3) {
-        printf(
-            "Incorrect usage!\n"
-            "Use %s -h to show help message:\n",
-        argv[0]);
+    char specfiedFormat = 0;
+    char specfiedValue = 0;
+
+    for (int i=1; i<argc; i++) {
+        if (specfiedValue) {
+            unsigned char value = (unsigned char) strtol(argv[i], NULL, 16);
+
+            if (value < 1 || value > 3) {
+                printf("Error: %d is not a valid value to write\n", value);
+                return NULL;
+            }
+
+            specfiedValue = 0;
+            args.writeValue = value;
+            continue;
+        }
+        
+        if (specfiedFormat) {
+            if (strcmp("waybar", argv[i]) == 0) {
+                args.format = waybar;
+            } else if (strcmp("json", argv[i]) == 0) {
+                args.format = json;
+            } else {
+                printf("%s is not a valid format.", argv[i]);
+                return NULL;
+            }
+
+            specfiedFormat = 0;
+            continue;
+        }
+
+        if (strcmp("-h", argv[i]) == 0) {
+            args.opMode = help;
+            return &args;
+        }
+
+        if (strcmp("-w", argv[i]) == 0) {
+            if (args.opMode != 99) {
+                printf("You cannot do multiple operation at the same time");
+                return NULL;
+            }
+            
+            specfiedValue = 1;
+            args.opMode = writeEC;
+            continue;
+        }
+
+        if (strcmp("-r", argv[i]) == 0) {
+            if (args.opMode != 99) {
+                printf("You cannot do multiple operation at the same time");
+                return NULL;
+            }
+
+            args.opMode = readEC;
+            continue;
+        }
+
+        if (strcmp("-f", argv[i]) == 0) {
+            specfiedFormat = 1;
+            continue;
+        }
+
+        if (strcmp("-t", argv[i]) == 0) {
+            if (args.opMode != 99) {
+                printf("You cannot do multiple operation at the same time");
+                return NULL;
+            }
+
+            args.opMode = toggleEC;
+            continue;
+        }
+
+    }
+
+    if (specfiedValue) {
+        printf("Write value is not specified\n");
+        return NULL;
+    }
+
+    return &args;
+
+}
+
+void printWaybar(unsigned char value) {
+    char *icons[] = {"󰾅", "󰾆", "󰓅"};
+    char *tooltips[] = {
+        "Balance", "Slient", "Performance"
+    };
+
+    printf("{\"value\": %d, \"text\": \"%s\", \"tooltip\": \"Fan Mode: %s\"}\n",
+        value,
+        icons[(value-1)%3],
+        tooltips[(value-1)%3]
+    );
+}
+
+int main(int argc, char *argv[]) {
+    // char *argvss[] = {"utils", "-f", "waybar", "-r"};
+    args_t *args = parseArgs(argc, argv);
+
+    if (args == NULL) {
+        printf("Use %s -h to show help message\n", argv[0]);
         return 1;
     }
 
-    if (strcmp(argv[1], "-h") == 0) {
-        show_help();
-        return 0;
+    unsigned char value;
+    unsigned char newValue;
+    switch ((*args).opMode) {
+
+        case writeEC:
+            ecWrite((*args).writeValue);
+            printf("Successfully set EC RAM 0x%02X to 0x%02X\n", EC_OFFSET, (*args).writeValue);
+            return 0;
+
+        case toggleEC:
+            value = ecRead();
+
+            if (value == 0xFF) {
+                printf("Error reading the EC value\n");
+                return 1;
+            }
+
+            switch (value) {
+                case BALANCE_MODE: 
+                    newValue = PERFORMANCE_MODE;
+                    break;
+                case SLIENT_MODE: 
+                    newValue = BALANCE_MODE;
+                    break;
+                case PERFORMANCE_MODE:
+                    newValue = SLIENT_MODE;
+                    break;
+            }
+            ecWrite(newValue);
+
+        case readEC:
+            value = ecRead();
+
+            if (value == 0xFF) {
+                printf("Error reading the EC value\n");
+                return 1;
+            }
+
+            if ((*args).format == waybar) {
+                printWaybar(value);
+                return 0;
+            }
+
+            printf("EC RAM value=%d\n", value);
+            return 0;
+
+        default:
+            showHelp(argv[0]);
     }
-
-    if (strcmp(argv[1], "-w") == 0) {
-        if (argc < 3) {
-            printf("Incorrect usage! Write value not specified.\n");
-            return 1;
-        }
-
-        unsigned char value = (unsigned char) strtol(argv[2], NULL, 16);
-
-        if (value < 1 || value > 3) {
-            printf("Incorrect usage! The write value must be 1, 2 or 3.\n");
-            return 1;
-        }
-
-        ec_write(value);
-        printf("Successfully set EC RAM 0x%02X to 0x%02X\n", EC_OFFSET, value);
-
-        return 0;
-    }
-
-    if (strcmp(argv[1], "-r") == 0) {
-        
-        unsigned char value = ec_read();
-
-        printf(
-            "{\n"
-            "  \"value\": \"%d\"\n"
-            "}\n",
-        value);
-        
-        return 0;
-    }
-
-    show_help();
 
     return 0;
 }
